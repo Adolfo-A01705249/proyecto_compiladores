a0fa518a34e06e3d17dc8801acd493f859489aba
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2022-09-09 13:01:34 -0500

    feat: read from std input and document functions

--- simplerparser1.py
+++ simplerparser1.py
@@ -1,43 +1,56 @@
-# Generates a text file with a list of grammar terminals and non-terminals 
+# Prints a list of a grammar's terminals and non-terminals
 # given an ENBF grammar
 
-# usage: $python coolerparser1.py <input file route>
+# usage: $python parser.py < <input file>
 
-# Author: Adolfo Acosta Castro
-# Date 2022/09/06
-
-import sys
+# Author: Adolfo Acosta Castro [A01705249]
+# Date: 2022/09/09
 
 def printSet(set):
+    """
+    Creates a string made of the elements of a set separated by commas
+    Arguments:
+        set: a python set
+    Returns:
+        The created string with the list of set elements
+    """
     resultString = ""
     SEPARATOR = ", "
     for item in set:
         resultString += item + SEPARATOR
     return resultString[:-len(SEPARATOR)]
 
-def parseLine(line, headerTokens, productionTokens):
+def parseLine(line, headerTokens, bodyTokens):
+    """
+    Takes the head and production tokens from one grammar production
+    and adds them to their respective sets
+    Arguments:
+        line: a string representing a grammar production
+        headerTokens: a set for the tokens on production headers
+        bodyTokens: a set for the tokens on production bodies
+    """
     tokens = line.split()
     headerTokens.add(tokens[0])
     for i in range(2, len(tokens)):
         if tokens[i] == '\'':
             continue
-        productionTokens.add(tokens[i])
+        bodyTokens.add(tokens[i])
 
-inputFileName = sys.argv[1]
-inputFile = open(inputFileName, 'r')
-n = int(inputFile.readline().strip())
+
+n = int(input().strip())
 
 headerTokens = set()
-productionTokens = set()
+bodyTokens = set()
 
+# Process each production one by one
 for i in range(n):
-    line = inputFile.readline().strip()
-    parseLine(line, headerTokens, productionTokens)
-inputFile.close()
+    line = input().strip()
+    parseLine(line, headerTokens, bodyTokens)
 
+# If a token appears on a production header then it is not terminal
+# even if it appears on a production body
 for token in headerTokens:
-    productionTokens.discard(token)
+    bodyTokens.discard(token)
 
-# Print results to console
-print(f"Terminal: {printSet(productionTokens)}")
+print(f"Terminal: {printSet(bodyTokens)}")
 print(f"Non terminal: {printSet(headerTokens)}")
